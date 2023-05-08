d297323be9dff0aaadd7ebda05769f3adb75ce0a
Carlox <cmarinbotella@gmail.com>
2023-05-08 10:40:51 +0200

    Cambios psswd Verify

--- TASCA GUIADA TDD/src/PasswordVerify/PasswordVerify.java
+++ TASCA GUIADA TDD/src/PasswordVerify/PasswordVerify.java
@@ -1,29 +1,30 @@
 package PasswordVerify;
 
 public class PasswordVerify {
-    public static void main(String[] args) {
-
-    }
-
+    
     public static boolean verifyPassword(String password) {
         // 8 caracteres
         // 2 numeros
-
+        
         if (password.length() >= 8) {
-            int contador = 0;
+            int contadornum = 0;
+            int capSignos = 0;
+            int capCount = 0;
             for (int i = 0; i < password.length(); i++) {
                 if (Character.isDigit(password.charAt(i))) {
-                    contador++;
+                    contadornum++;
+                } else if (((Password.charAt(i) > 32 && Password.charAt(i) < 48) || (Password.charAt(i) > 63 && Password.charAt(i) < 65)) {
+                    capSignos++;
+                } else if ((Password.charAt(i) > 64 && Password.charAt(i) < 91)) {
+                    capCount++;
                 }
             }
-
-            if (contador >= 2) {
-                return true;
-            }
-        } else {
-
         }
-
-        return false;
+        
+        if (contadornum >= 2 && capSignos >= 1 && capCount >= 1) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
