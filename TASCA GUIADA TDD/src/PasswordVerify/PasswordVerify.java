7e7132217818c81f5df160272c1326e129acd8f5
Ferran <fmontblanch@gmail.com>
2023-05-08 10:23:34 +0200

    Pass word Verify

---
+++ TASCA GUIADA TDD/src/PasswordVerify/PasswordVerify.java
@@ -0,0 +1,27 @@
+package PasswordVerify;
+
+public class PasswordVerify {
+    public static void main(String[] args) {
+
+    }
+
+    public static boolean verifyPassword(String password) {
+        // 8 caracteres
+        // 2 numeros
+
+        if (password.length() >= 8) {
+            int contador = 0;
+            for (int i = 0; i < password.length(); i++) {
+                if (Character.isDigit(password.charAt(i))) {
+                    contador++;
+                }
+            }
+
+            if (contador >= 2) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
