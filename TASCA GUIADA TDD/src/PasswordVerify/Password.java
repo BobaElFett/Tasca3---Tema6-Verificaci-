a52caea7e2a0cba1760df877607ea2e9bfbd6d56
Ferran <fmontblanch@gmail.com>
2023-05-08 11:49:05 +0200

    Password Verify complet i Password Complet

--- TASCA GUIADA TDD/src/PasswordVerify/Password.java
+++ TASCA GUIADA TDD/src/PasswordVerify/Password.java
@@ -4,12 +4,24 @@ public class Password {
     private String error;
     private Boolean valid;
 
-    public Password(String error, Boolean valid) {
-        this.error = error;
-        this.valid = valid;
+    public Password() {
+        this.error = "";
+        this.valid = false;
     }
 
     public void actualizarError(String errorNou) {
-        this.error += "\n" + errorNou;
+        if (this.error.equals("")) {
+            this.error = errorNou;
+        } else {
+            this.error += "\n" + errorNou;
+        }
+    }
+
+    public String getError() {
+        return this.error;
+    }
+
+    public void setValid(Boolean valid) {
+        this.valid = valid;
     }
 }
--- TASCA GUIADA TDD/src/PasswordVerify/PasswordVerify.java
+++ TASCA GUIADA TDD/src/PasswordVerify/PasswordVerify.java
@@ -2,24 +2,41 @@ package PasswordVerify;
 
 public class PasswordVerify {
     
-    public static boolean verifyPassword(String contrasena) {
-        // 8 caracteres
-        // 2 numeros
-        int contadornum = 0;
-        int capSignos = 0;
-        int capCount = 0;
-        if (contrasena.length() >= 8) {
-            for (int i = 0; i < contrasena.length(); i++) {
-                if (Character.isDigit(contrasena.charAt(i))) {
-                    contadornum++;
-                } else if (((contrasena.charAt(i) > 32 && contrasena.charAt(i) < 48) || (contrasena.charAt(i) == 64))) {
-                    capSignos++;
-                } else if ((contrasena.charAt(i) > 64 && contrasena.charAt(i) < 91)) {
-                    capCount++;
-                }
+    public static Password verifyPassword(String contrasena) {
+
+        Password pass = new Password();
+
+        int contadornum = 0, capSignos = 0, capCount = 0;
+        if (contrasena.length() < 8) {
+            pass.actualizarError("La contrasenya ha de tenir almenys 8 caràcters");
+        }
+
+        for (int i = 0; i < contrasena.length(); i++) {
+            if (Character.isDigit(contrasena.charAt(i))) {
+                contadornum++;
+            } else if (((contrasena.charAt(i) > 32 && contrasena.charAt(i) < 48) || (contrasena.charAt(i) == 64))) {
+                capSignos++;
+            } else if ((contrasena.charAt(i) > 64 && contrasena.charAt(i) < 91)) {
+                capCount++;
             }
         }
-    
-        return contadornum >= 2 && capSignos >= 1 && capCount >= 1;
+
+        if (contadornum < 2) {
+            pass.actualizarError("La contrasenya ha de contenir almenys 2 números");
+        }
+
+        if (capCount >= 1) {
+            pass.actualizarError("La contrasenya ha de contenir almenys una lletra majúscula");
+        }
+
+        if (capSignos >= 1) {
+            pass.actualizarError("La contrasenya ha de contenir almenys un caràcter especial");
+        }
+
+        if (pass.getError().equals("")) {
+            pass.setValid(true);
+        }
+
+        return pass;
     }
 }
